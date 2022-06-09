347db9ce44b7ad65bf191601e2871ee2e9d8cebb
fadi <fadi99.email@gmail.com>
2022-06-09 11:37:27 +0700

    Add search functionality

--- src/containers/SearchForm.js
+++ src/containers/SearchForm.js
@@ -1,4 +1,5 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 import SearchIcon from "@mui/icons-material/Search";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
@@ -6,29 +7,43 @@ import MicIcon from "@mui/icons-material/Mic";
 
 import Button from "../components/Button";
 import SearchBar from "../components/SearchBar";
+import formatSearchQuery from "../utils/formatSearchQuery";
 
 const SearchForm = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setSearch("");
+  }, [pathname]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchBy = pathname.substring(1) ? pathname.substring(1) : "search";
+    navigate(`/search-result?by=${searchBy}&q=${formatSearchQuery(search)}`);
+  };
 
   return (
-    <form className="mx-auto max-w-xl">
-      {/* Searchbar */}
-      <SearchBar>
+    <form onSubmit={handleSubmit} className="mx-auto max-w-xl">
+      <SearchBar search={search} setSearch={setSearch}>
         {pathname === "/images" && (
           <CameraAltIcon className="cursor-pointer" color="action" />
         )}
         <MicIcon className="cursor-pointer" color="primary" />
         {pathname !== "/" && (
-          <button type="submit">
+          <button type="submit" onClick={handleSubmit}>
             <SearchIcon className="cursor-pointer" color="primary" />
           </button>
         )}
       </SearchBar>
 
-      {/* Buttons */}
       {pathname === "/" && (
         <div className="flex justify-center space-x-3">
-          <Button type="submit">Google Search</Button>
+          <Button type="submit" onClick={handleSubmit}>
+            Google Search
+          </Button>
           <Button type="button">I'm Feeling Lucky</Button>
         </div>
       )}
---
+++ src/utils/formatSearchQuery.js
@@ -0,0 +1,5 @@
+const formatSearchQuery = (search) => {
+  return search.split(" ").join("+");
+}
+
+export default formatSearchQuery;
