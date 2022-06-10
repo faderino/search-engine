6ce998ee40fc9115a1c96ef0abfed9fbe0285154
fadi <fadi99.email@gmail.com>
2022-06-10 14:59:56 +0700

    Slice tab navigation into component

---
+++ src/components/TabNavigation.js
@@ -0,0 +1,49 @@
+import { useLocation } from "react-router-dom";
+
+import ImageOutlinedIcon from "@mui/icons-material/ImageOutlined";
+import NewspaperOutlinedIcon from "@mui/icons-material/NewspaperOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+import { getCurrentPath } from "../utils/getCurrentPath";
+
+import TabNavItem from "./TabNavItem";
+
+const TabNavigation = ({ search, navigate }) => {
+  const { pathname } = useLocation();
+  const searchBy = getCurrentPath(pathname);
+
+  return (
+    <nav className="border-b pl-8 pr-8 xl:pl-44">
+      <div className="flex space-x-6">
+        <TabNavItem
+          title="All"
+          search={search}
+          searchBy={searchBy}
+          navigate={navigate}
+        >
+          <SearchIcon fontSize="inherit" />
+        </TabNavItem>
+
+        <TabNavItem
+          title="Images"
+          search={search}
+          searchBy={searchBy}
+          navigate={navigate}
+        >
+          <ImageOutlinedIcon fontSize="inherit" />
+        </TabNavItem>
+
+        <TabNavItem
+          title="News"
+          search={search}
+          searchBy={searchBy}
+          navigate={navigate}
+        >
+          <NewspaperOutlinedIcon fontSize="inherit" />
+        </TabNavItem>
+      </div>
+    </nav>
+  );
+};
+
+export default TabNavigation;
--- src/pages/SearchResultPage.js
+++ src/pages/SearchResultPage.js
@@ -1,31 +1,21 @@
 import { useState } from "react";
-import {
-  Outlet,
-  useLocation,
-  useNavigate,
-  useSearchParams,
-} from "react-router-dom";
+import { Outlet, useNavigate, useSearchParams } from "react-router-dom";
 
 import CloseIcon from "@mui/icons-material/Close";
-import ImageOutlinedIcon from "@mui/icons-material/ImageOutlined";
 import MicIcon from "@mui/icons-material/Mic";
-import NewspaperOutlinedIcon from "@mui/icons-material/NewspaperOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 
 import avatar from "../assets/avatar.png";
 import Avatar from "../components/Avatar";
-import { getCurrentPath } from "../utils/getCurrentPath";
 import logo from "../assets/googlelogo.png";
 import SearchBar from "../components/SearchBar";
-import TabNavItem from "../components/TabNavItem";
 import Footer from "../components/Footer";
+import TabNavigation from "../components/TabNavigation";
 
 const SearchResultPage = () => {
   const navigate = useNavigate();
-  const { pathname } = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get("q");
-  const searchBy = getCurrentPath(pathname);
 
   const [search, setSearch] = useState(query);
 
@@ -75,37 +65,7 @@ const SearchResultPage = () => {
         <Avatar width={32} src={avatar} className="hidden sm:inline-block" />
       </div>
 
-      {/* Tab Navigation */}
-      <nav className="border-b pl-8 pr-8 xl:pl-44">
-        <div className="flex space-x-6">
-          <TabNavItem
-            title="All"
-            search={search}
-            searchBy={searchBy}
-            navigate={navigate}
-          >
-            <SearchIcon fontSize="inherit" />
-          </TabNavItem>
-
-          <TabNavItem
-            title="Images"
-            search={search}
-            searchBy={searchBy}
-            navigate={navigate}
-          >
-            <ImageOutlinedIcon fontSize="inherit" />
-          </TabNavItem>
-
-          <TabNavItem
-            title="News"
-            search={search}
-            searchBy={searchBy}
-            navigate={navigate}
-          >
-            <NewspaperOutlinedIcon fontSize="inherit" />
-          </TabNavItem>
-        </div>
-      </nav>
+      <TabNavigation search={search} navigate={navigate} />
 
       {/* Search Result */}
       <div className="ml-8 grow xl:ml-44">
