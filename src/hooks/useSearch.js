e7598e6cc0dd4302299a697aa6b49909b92cb267
fadi <fadi99.email@gmail.com>
2022-06-10 11:08:01 +0700

    Add hook's description

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -3,14 +3,24 @@ import { useSearchParams } from "react-router-dom";
 import { formatSearchQuery } from "../utils/formatSearchQuery";
 import { getNormalSearch } from "../api/normalSearch";
 
+/**
+ * Hooks for making request to google search API based on current query params URL
+ * @param {string} type accepts NORMAL_SEARCH || IMAGE_SEARCH || NEWS_SEARCH
+ * @returns state object with "loading" and "data" property
+ */
 export const useSearch = (type) => {
   const [searchParams] = useSearchParams();
   const query = formatSearchQuery(searchParams.get("q"));
 
   const [state, setState] = useState({ status: "pending", data: null });
 
-  const searchApi =
-    type === "NORMAL_SEARCH" ? getNormalSearch : getNormalSearch;
+  let searchApi;
+  switch (type) {
+    case "NORMAL_SEARCH":
+    default:
+      searchApi = getNormalSearch;
+      break;
+  }
 
   useEffect(() => {
     setState((state) => ({ ...state, status: "pending" }));
