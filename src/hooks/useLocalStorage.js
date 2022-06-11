97d4b1b6cddc1883a13c7564dae678bc0b5bce0e
fadi <fadi99.email@gmail.com>
2022-06-11 21:17:42 +0700

    Add LocalStorage hooks for reading list

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+/**
+ * Hooks to get and set value to local storage.
+ * @param {string} key 
+ * @param {*} defaultValue 
+ * @returns {array} [state<any>, setState<function>]
+ */
+export const useLocalStorage = (key, defaultValue = "") => {
+  const [state, setState] = useState(() => {
+    const inLocalStorage = localStorage.getItem(key);
+    if (inLocalStorage) {
+      return JSON.parse(inLocalStorage);
+    }
+    return defaultValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+
+  return [state, setState];
+};
