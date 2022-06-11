1408adcf107d6b32971636e99b3187b8d8143d8c
fadi <fadi99.email@gmail.com>
2022-06-11 23:28:00 +0700

    Add navigation from news result to reading list page

--- src/containers/NewsResult.js
+++ src/containers/NewsResult.js
@@ -5,8 +5,10 @@ import { useLocalStorage } from "../hooks/useLocalStorage";
 import NewsCard from "../components/NewsCard";
 import Loading from "../components/Loading";
 import Pagination from "../components/Pagination";
+import { useNavigate } from "react-router-dom";
 
 const NewsResult = () => {
+  const navigate = useNavigate();
   const { loading, data } = useSearch("NEWS_SEARCH");
   const {
     currentPage,
@@ -26,6 +28,15 @@ const NewsResult = () => {
 
   return (
     <div className="mr-8 py-8 pt-4">
+      <nav className="mb-2 flex max-w-xl items-center justify-between">
+        <h1 className="text-lg font-semibold">Headlines</h1>
+        <button
+          onClick={() => navigate("/reading-list")}
+          className="text-sm font-semibold text-blue-500"
+        >
+          My Reading List
+        </button>
+      </nav>
       {displayNews.map((news) => (
         <NewsCard
           key={news.id}
